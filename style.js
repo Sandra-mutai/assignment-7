9995f07ea1b095f0b10b49ecb7f6d74a78fe2712
sandra-mutai <sandramuttai@gmail.com>
2021-04-06 11:32:24 +0300

    seventh commit

---
+++ style.js
@@ -0,0 +1,21 @@
+var a;
+typeof a;
+console.log(typeof (a));
+
+a="hello world";
+typeof a;
+console.log(typeof(a))
+
+a=42;
+typeof a;
+console.log(typeof(a))
+
+a=true;
+typeof a;
+console.log(typeof(a))
+
+a=null;
+typeof a;
+console.log(typeof(a))
+
+
